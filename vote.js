523f89e3d7ee6ece11b0a6d2c379b89036357615
KISHAN YADAV <kishanyadav2093@gmail.com>
2026-05-06 23:41:06 +0530

    fix: specify chromium executable path to resolve launch failures in vote-bot

--- vote.js
+++ vote.js
@@ -5,6 +5,7 @@ const puppeteer = require('puppeteer');
     try {
 
         const browser = await puppeteer.launch({
+            executablePath: '/usr/bin/google-chrome',
             headless: "new",
             args: [
                 '--no-sandbox',
